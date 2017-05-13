1eb879b17ec7a5b2ca014b75ba3fbf881e859e38
Vu Duc Tuyen <tuyen@naxam.net>
2017-05-13 15:37:42 +0700

    Update SDK to v3.5.4

--- Naxam.Mapbox.iOS/Properties/AssemblyInfo.cs
+++ Naxam.Mapbox.iOS/Properties/AssemblyInfo.cs
@@ -16,7 +16,7 @@ using Foundation;
 [assembly: AssemblyDescription("Xamarin binding library for Mapbox iOS SDK")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("NAXAM CO.,LTD")]
-[assembly: AssemblyProduct("OSS")]
+[assembly: AssemblyProduct("X Bindings")]
 [assembly: AssemblyCopyright("Copyright @ 2017 NAXAM")]
 [assembly: AssemblyTrademark("NAXAM")]
 [assembly: AssemblyCulture("")]
@@ -25,8 +25,8 @@ using Foundation;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("3.5.2")]
-[assembly: AssemblyInformationalVersion("3.5.2-alpha")]
+[assembly: AssemblyVersion("3.5.4")]
+[assembly: AssemblyInformationalVersion("3.5.4")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
